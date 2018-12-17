7b86ddf204b73ef5fde95fb23434547915c02cb2
arwaalotaibi <arwa@arwas-MacBook-Pro.local>
2018-12-17 18:34:41 +0300

    “task14”

--- api/views.py
+++ api/views.py
@@ -1,6 +1,15 @@
 # Your imports should be here
-
-
+from rest_framework.generics import ListAPIView
+from restaurants.models import Restaurant
+from .serializers import RestaurantListSerializer
+from django.http import JsonResponse
 # Complete me! I should be your APIListView
-class RestaurantListView():
-	...
+
+class RestaurantListView(ListAPIView):
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantListSerializer
+
+def resturant_list_api(request):
+	restaurants = Restaurant.objects.all()
+	json_restaurants =  RestaurantListSerializer(restaurants ,many=true).data
+	return JsonResponse(json_restaurants, safe=False)
--- task_14/urls.py
+++ task_14/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from restaurants import views
 from api.views import RestaurantListView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
